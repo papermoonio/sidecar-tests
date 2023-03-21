f52c55c008e15b80303e28816937e8c32ac65655
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-03-21 09:44:10 -0500

    Fixed Moonriver issue

--- monitor.py
+++ monitor.py
@@ -89,7 +89,9 @@ def perform_content_test():
     runtimeVersion = substrate.get_block_runtime_version(resjson['hash'])['specVersion']
 
     # If the runtimeVersion is 2100 or greater (in Moonbase Alpha), then we base the baseGasFee off of it
-    if (runtimeVersion >= 2100 and args.network == "moonbase-alpha"):
+    isDynamicFeeMoonbaseAlpha = runtimeVersion >= 2100 and args.network == "moonbase-alpha"
+    isDynamicFeeMoonriver = runtimeVersion >= 2201 and args.network == "moonriver"
+    if (isDynamicFeeMoonbaseAlpha or isDynamicFeeMoonriver):
       feeMultiplierRes, error = fetch_sidecar_api(f"/pallets/transaction-payment/storage/nextFeeMultiplier?at={str(int(blockNum))}")
       # Calculation derived from https://github.com/PureStake/moonbeam/blob/c87469ad8740a97fe2fbc763a25a9e209cb89baf/runtime/moonbase/src/lib.rs#L406
       baseGasFee = int((int(feeMultiplierRes.json()['value']) * 50000 * 25000) / 1000000000000000000)
